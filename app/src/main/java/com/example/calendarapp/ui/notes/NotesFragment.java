be7c2bd758cde2898a43c2c9e6eecdf6e515b842
jlf4509 <jlf4509@g.rit.edu>
2020-11-05 14:38:23 -0500

    v3 - working on notes

--- app/src/main/java/com/example/calendarapp/ui/notes/NotesFragment.java
+++ app/src/main/java/com/example/calendarapp/ui/notes/NotesFragment.java
@@ -4,7 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,9 +17,17 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.calendarapp.R;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.FileOutputStream;
+
 public class NotesFragment extends Fragment {
 
     private NotesViewModel notesViewModel;
+    private EditText notes;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,12 +35,62 @@ public class NotesFragment extends Fragment {
                 new ViewModelProvider(this).get(NotesViewModel.class);
         View root = inflater.inflate(R.layout.fragment_notes, container, false);
 //        final TextView textView = root.findViewById(R.id.text_notifications);
+        this.notes = root.findViewById(R.id.enterNotes);
+        final Button save = root.findViewById(R.id.buttonSave);
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Save("Note1.txt");
+            }
+        });
         notesViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-//                textView.setText(s);
+
             }
         });
+        this.notes.setText(Open("Note1.txt"));
         return root;
+    }
+
+    public void Save(String fileName) {
+        try {
+
+            OutputStreamWriter out =
+                    new OutputStreamWriter(openFileOutput(fileName, 0));
+            out.write(notes.getText().toString());
+            out.close();
+//            Toast.makeText(this, "Note saved!", Toast.LENGTH_SHORT).show();
+        } catch (Throwable t) {
+//            Toast.makeText(this, "Exception: " + t.toString(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public String Open(String fileName) {
+        String content = "";
+        if (FileExists(fileName)) {
+            try {
+                InputStream in = openFileInput(fileName);
+                if ( in != null) {
+                    InputStreamReader tmp = new InputStreamReader( in );
+                    BufferedReader reader = new BufferedReader(tmp);
+                    String str;
+                    StringBuilder buf = new StringBuilder();
+                    while ((str = reader.readLine()) != null) {
+                        buf.append(str + "\n");
+                    } in .close();
+                    content = buf.toString();
+                }
+            } catch (java.io.FileNotFoundException e) {} catch (Throwable t) {
+//                Toast.makeText(this, "Exception: " + t.toString(), Toast.LENGTH_LONG).show();
+            }
+        }
+        return content;
+        return "";
+    }
+
+    public boolean FileExists(String fname) {
+        File file = getBaseContext().getFileStreamPath(fname);
+        return file.exists();
     }
 }
